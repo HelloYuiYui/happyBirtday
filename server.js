16fc3399a77722ca0f13070c271f8b6535c0ea3c
Enes IA <enes35ihsan@gmail.com>
2020-05-11 04:09:50 +0100

    compatible with heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log(Quad.encode(42, 87, "afdckjsef"));
 var port = 3000;
 
 app.get("/", function(req, res){
-    res.sendFile("index.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.get("/:vals", function(req, res) {
@@ -22,36 +22,7 @@ app.get("/:vals", function(req, res) {
     if (n.indexOf("+") > -1) {
         n = n.split("+").join(" ");
     }
-    res.send(`
-        <html>
-            <head>
-                <title>Happy BDay</title>
-                <link rel="icon" href="https://static.thenounproject.com/png/33119-200.png">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-                <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">
-                <meta charset="utf-8">
-                <script src="script.js"></script>
-                <style>
-                    body {
-                        background-color: #${c};
-                        height: 20vh;
-                        width: 100vw;
-                    }
-
-                    h1 {
-                        text-align: center;
-                        margin-top: 40vh;
-                        font-size: 52px;
-                        text-shadow: 0px 0px 15px rgba(255, 255, 255, 1);
-                        font-family: "Lobster Two", cursive;
-                    } 
-                </style>
-            </head>
-            <body>
-                <h1 id="name"><i class="fa fa-birthday-cake"></i> Happy Birthday ${decodeURI(n)} <i class="fa fa-birthday-cake"></i></h1>
-            </body>
-        </html>
-`);
+    res.send('<html><head><title>Happy BDay</title><link rel="icon" href="https://static.thenounproject.com/png/33119-200.png"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet"><meta charset="utf-8"><script src="script.js"></script><style>body {background-color: #${c};height: 20vh;width: 100vw;}h1 {text-align: center;margin-top: 40vh;font-size: 52px;text-shadow: 0px 0px 15px rgba(255, 255, 255, 1);font-family: "Lobster Two", cursive;}</style></head><body><h1 id="name"><i class="fa fa-birthday-cake"></i> Happy Birthday ${decodeURI(n)} <i class="fa fa-birthday-cake"></i></h1></body></html>');
 })
 
 app.set('port', process.env.PORT || 80);

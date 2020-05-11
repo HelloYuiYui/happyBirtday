bd431ad4290a85a18669f78da31bffa9b9db9ba0
Enes IA <enes35ihsan@gmail.com>
2020-05-11 02:29:34 +0100

    added a server side .js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 function getParams() {
-    var desire = location.search.slice(1).split("&");//[0];//.split("=");
-    var colour = desire[0].split("=")[1];
-    var name = desire[1].split("=")[1];
+    console.log(name, colour);
+    
+    /*
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+         if (xhr.readyState==4 && xhr.status==200){
+             var resp = xhr.responseText.split('_');
+             var name = resp[0];
+             var colour = resp[1];
+             personalise(name, colour);
+         }
+    };
+        
+    xhr.open("GET", "/" + name + "_" + colour);
+    xhr.send();
+    */
+    
+    //document.getElementById("name").innerHTML = '<i class="fa fa-birthday-cake"></i> Happy Birthday ' + (decodeURI(name)) + ' <i class="fa fa-birthday-cake"></i>';
+    //document.getElementsByTagName("body")[0].style.backgroundColor = "#" + colour;
+    
+}
+
+function personalise(n, c){
     if (name.indexOf("+") > -1) {
-        var name = name.split("+").join(" ");
+        name = name.split("+").join(" ");
     }
-    document.getElementById("name").innerHTML = '<i class="fa fa-birthday-cake"></i> Happy Birthday ' + name + ' <i class="fa fa-birthday-cake"></i>';
-    document.getElementsByTagName("body")[0].style.backgroundColor = "#" + colour;
+    document.getElementById("name").innerHTML = '<i class="fa fa-birthday-cake"></i> Happy Birthday ' + (decodeURI(n)) + ' <i class="fa fa-birthday-cake"></i>';
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#" + c;
 }
 
 function nav(){
     var c = document.getElementById("favcolor").value.slice(1);
     var n = document.getElementById("name").value.split(" ").join("+");
-    window.location = ("message.html?c=" + c + "&n=" + n);
+    location.replace(n + "_" c);
 }
+
+// location.replace((document.getElementById('name').value.split('').join('+')), (document.getElementById('favcolor').value.slice(1)))
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var Quad = require("quadratic-encryptor");
+// var dotenv = require("dotenv");
+// var colors = require("colors");
+
+app.use(express.static(__dirname + '/'));
+
+console.log(Quad.encode(42, 87, "afdckjsef"));
+
+var port = 3000;
+
+app.get("/", function(req, res){
+    res.sendFile("index.html");
+})
+
+app.get("/:vals", function(req, res) {
+    console.log(req.params.vals);
+    var n = (req.params.vals).split("_")[0];
+    var c = (req.params.vals).split("_")[1];
+    if (n.indexOf("+") > -1) {
+        n = n.split("+").join(" ");
+    }
+    res.send(`
+        <html>
+            <head>
+                <title>Happy BDay</title>
+                <link rel="icon" href="https://static.thenounproject.com/png/33119-200.png">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">
+                <meta charset="utf-8">
+                <script src="script.js"></script>
+                <style>
+                    body {
+                        background-color: #${c};
+                        height: 20vh;
+                        width: 100vw;
+                    }
+
+                    h1 {
+                        text-align: center;
+                        margin-top: 40vh;
+                        font-size: 52px;
+                        text-shadow: 0px 0px 15px rgba(255, 255, 255, 1);
+                        font-family: "Lobster Two", cursive;
+                    } 
+                </style>
+            </head>
+            <body>
+                <h1 id="name"><i class="fa fa-birthday-cake"></i> Happy Birthday ${decodeURI(n)} <i class="fa fa-birthday-cake"></i></h1>
+            </body>
+        </html>
+`);
+})
+
+http.listen(port, function(){
+    console.log("listening on :" + port);
+})
